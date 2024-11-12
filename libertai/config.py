0dfc527b6f69ae26f8d1498dd056eaa466f300c5
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-11-12 10:18:54 +0700

    feat: production API URL

--- libertai/config.py
+++ libertai/config.py
@@ -6,7 +6,7 @@ class _Config:
 
     def __init__(self):
         self.AGENTS_BACKEND_URL = os.getenv("LIBERTAI_CLIENT_AGENTS_BACKEND_URL",
-                                            "https://e6cd-58-142-26-96.ngrok-free.app")
+                                            "https://agent.api.libertai.io")
 
 
 config = _Config()
